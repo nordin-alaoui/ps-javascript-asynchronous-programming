768b190e83c18c83ceb0c4d983e670ad13c6f225
Nordin-MA <83017868+Nordin-MA@users.noreply.github.com>
2024-06-09 22:34:40 +0200

    Awaiting parallel calls

--- public/orders.js
+++ public/orders.js
@@ -1,23 +1,16 @@
-async function get(){
-  try{
-    const statuseReq = axios.get("http://localhost:3000/api/orderStatuses");
-    const orderReq = axios.get("http://localhost:3000/api/orders");
 
-    const {data: statuses} = await statuseReq;
-    const {data: order} = await orderReq;
-    
-    const orders = order.map(o => {
-      return {
-        ...o,
-        orderStatus: statuses.find(d => d.id === o.orderStatusId).description
-      };
-    });
-    showOrderList("#order-list", orders);
-  }
-  catch(err)
-  {
-    showError("order-list", err)
-  }
+async function get(){
+
+  await Promise.all([
+    (async () => {
+      const {data: statuses} = await axios.get("http://localhost:3000/api/orderStatuses");
+      showMessage("statuses Fetched");
+    })(),
+    (async () => {
+    const {data: data} = await axios.get("http://localhost:3000/api/orders");;
+      showOrderList("#order-list", data);
+    })()
+  ]) 
 }
 
 get();
